325093b8b0b467c412ab6e0040d115d8b80e3911
James Joyce <16JJoyce@stjosephsrush.com>
2018-01-21 13:12:09 +0000

    More variables.py
    lots and lots and lots and lots of varying variables that are very varied.

---
+++ MORE variables.py
@@ -0,0 +1,57 @@
+from time import sleep
+import random
+
+def ham():
+  print("calories=150")
+   
+def milk():
+  print("If you have 3 litres, then calories = 3000. ")
+  print("If you have 2 litres, then calories = 2300. ")
+ 
+def egg():
+  print("1 Egg is 155 calories. A dozen eggs is 936 calories. ")
+ 
+def banana():
+  print("A banana is 89 calories.")
+  
+def cheese():
+  print("There is 402 calories in every 100 grams of cheddar cheese. ")
+  
+def butter():
+  print("There is 717 calories in every 100 grams of butter. ")
+  
+def ketchup():
+  print("There is 112 calories in every 100 grams of ketchup")
+  
+def coleslaw():
+  print("There is 152 calories in every 100 grams of store-bought coleslaw. ")
+  print("In every 100 grams of home-made coleslaw, there is only 78 calories. ")
+  
+def yoghurt():
+  print("There is 59 calories in every 100 grams of greek yoghurt. ")
+  print("In every 100 grams of frozen yoghurt, there is 160 calories. ")
+  
+def pepper():
+  print("There is 43 in a large red pepper. ")
+  print("In a large yellow pepper, there is 27 calories. ")
+  print("There is 20 calories in a large green pepper. ")
+  
+def mushroom():
+  print("There is 20 calories in every 100 grams of white mushrooms. ")
+  
+def salad_leaves():
+  print("There is 2 calories in one spinach leaf. ")
+  print("Rocket leaves have 0 calories. ")
+  
+def beetroot():
+  print("There is 35 calories in a single beetroot. ")
+  
+def lettuce():
+  print("There is 75 calories in a single head of iceberg lettuce. ")
+  
+def mayo():
+  print("There is 680 calories in every 100 grams of mayonnaise. ")
+
+def mustard():
+  print("There is 66 calories in every 100 grams of Heinz mustard. ")
+  print("In Dijon mustard, there is 160 calories in every 100 grams. ")
