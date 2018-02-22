d1068fbe259182f5688429122545fd5ea5a6fe2b
MeganC123 <16MCollins@stjosephsrush.ie>
2018-02-22 09:03:20 +0000

    Create ingredients.py

---
+++ ingredients.py
@@ -0,0 +1,40 @@
+ingredients=input("what would you like to make ?")
+
+
+
+if ingredients=="cookies":
+    print("Ok heres what you will need ")
+    print("125g butter, softened")
+    print("100g light brown soft sugar")
+    print("125g caster sugar")
+    print("1 egg, lightly beaten")
+    print("225g self-raising flour")
+    print("½ tsp salt")
+    print("200g chocolate chips")
+
+
+
+elif ingredients=="cupcakes":
+    print("ok heres what you will need")
+    print("2 medium eggs")
+    print("125ml vegetable oil")
+    print("250ml semi-skimmed milk")
+    print("250g golden caster sugar")
+    print("400g self-raising flour")
+    print("1 tsp salt")
+    print("100g chocolate chips")
+
+
+
+elif ingredients=="toasted sandwhich":
+    print("ok heres what you will need")
+    print("bread")
+    print("butter")
+    print("ham")
+    print("cheese")
+
+
+
+else:
+    print("unfortunately we do not have the reciepe for this")
+    
